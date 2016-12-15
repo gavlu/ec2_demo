e369b210b0059e5afb9cdaf367a492fc81d9027c
Gavin <gavinlue@gmail.com>
2016-12-15 17:32:49 -0500

    finish setup

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   apps : [
     {
-      name      : "EC2 Demo App",
-      script    : "index.js",
+      name      : 'EC2 Demo App',
+      script    : 'index.js',
       env: {
-        COMMON_VARIABLE: "true"
+        COMMON_VARIABLE: 'true'
       },
       env_production : {}
     }
@@ -12,12 +12,14 @@ module.exports = {
 
   deploy : {
     production : {
-      user : "user",
-      host : "ec2-52-91-181-50.compute-1.amazonaws.com",
-      ref  : "origin/master",
-      repo : "git@github.com:gavlu/ec2_demo.git",
-      path : "/home/ubuntu/test_app",
-      "post-deploy" : "npm install && pm2 startOrRestart ecosystem.json --env production"
+      user : 'ubuntu',
+      host : 'ec2-52-91-181-50.compute-1.amazonaws.com',
+      ref  : 'origin/master',
+      'pre-setup': 'ssh-add ~/.ssh/id_rsa',
+      key  : '/Users/gavinlue/.ssh/TestServerKey2.pem',
+      repo : 'git@github.com:gavlu/ec2_demo.git',
+      path : '/home/ubuntu/test_app',
+      'post-deploy' : 'npm install && pm2 startOrRestart ecosystem.json --env production'
     }
   }
 }
